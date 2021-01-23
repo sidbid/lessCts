7bf1c1db114be0efc0e2ee8ca1a424482a61f7d1
sidbid <elsidbid@gmail.com>
2021-01-23 10:35:46 -0800

    module information updated to require desktop, probably

--- lessCtsText/src/module-info.java
+++ lessCtsText/src/module-info.java
@@ -1,3 +1,8 @@
+/**
+ * The module for the text project of lessCts. It defines most of the tools that will be used in later modules(graphical, native), and also text based ui.
+ * @author Sid
+ *
+ */
 module lessCts.text {
 	requires java.base;
 	requires java.desktop;
